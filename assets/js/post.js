1f846fb76d8b3c3c21194c6b0c3567dd211ba798
Paul Pham <paul@aquaron.com>
2024-02-08 11:16:33 -0800

    feat: Decline and add a message

--- assets/js/post.js
+++ assets/js/post.js
@@ -86,58 +86,89 @@ function findNumber(n) {
   });
 }
 
-async function respond(e) {
+const restore = (e) => {
+  const divId = e?.dataset?.id;
+  document.getElementById(divId)
+    .querySelectorAll('input')
+    .forEach( i => i.disabled = false );
+  e.disabled = false;
+};
+
+async function respond(e, action) {
   const divId = e?.dataset?.id;
   const el = document.getElementById(divId);
   const ins = el.querySelectorAll('input');
   const data = { };
   const accepted = document.getElementById('accepted-content');
+  const declined = document.getElementById('declined-content');
   const error = document.getElementById('error-content');
 
   e.disabled = true;
 
-  const restore = () => {
-    ins.forEach( i => i.disabled = false );
-    e.disabled = false;
-  };
-
   ins.forEach( i => {
     i.classList.remove('error');
 
-    if (!i.value) {
+    if (!i.value && i.name !== 'message') {
       i.classList.add('error');
       return;
     }
+
     data[i.name] = i.value;
     i.disabled = true;
-  });
+  } );
 
-  const {name,phone,guests} = data;
+  const {name,phone,guests,message} = data;
 
   if (!name || !phone || !guests) {
-    restore();
+    restore(e);
     return;
   }
 
   const info = await lookup(phone);
 
   if (!info || !info.success) {
-    page(el, 'error', 'error-content', 'Sorry, I cannot find you on the guest list. Please contact Paul!');
-    restore();
+    page(el, 'error', 'error-content', `
+      Sorry, I cannot find you on the guest list.
+      Please contact Paul!
+    `);
+    restore(e);
     return;
   }
 
+  return {
+    db: info.data,
+    guests,
+    phone,
+    name,
+    message,
+  };
+}
+
+async function accept(e) {
+  const divId = e?.dataset?.id;
+  const el = document.getElementById(divId);
+  const info = await respond(e);
+
+  const { name, guests, phone, message, db: { short } } = info;
+  const id = ID(phone);
+
+  let salutation = 'Your';
+
+  if (short)
+    salutation = `Dear ${short}, your`;
+
   try {
-    await makeRequest('POST', `/accept/${ID(phone)}/${guests}`, {
-      message: `Name: ${name} -- Phone: ${phone}`,
+    await makeRequest('POST', `/accept/${id}/${guests}`, {
+      message: `${message || ''} -- Name: ${name} -- Phone: ${phone}`,
     });
     page(el, 'accepted', 'accepted-content', `
-      <h1 class='name'>${info.data.name}</h1>
+      <h1 class='name'>${name}</h1>
       <p>
         Thank you! You are confirmed for party of ${guests}.
       </p>
       <p>
-        Your presence will be a blessing and joy to our wedding.
+        ${salutation} presence will be a blessing to us!
+        We can't wait to celebrate this moment with you.
       </p>
 
       <p>
@@ -145,10 +176,49 @@ async function respond(e) {
         Paul & Kaylee
       </p>
     `);
-  } catch ({status}) {
+  } catch (er) {
     page(el, 'error', 'error-content', 'Sorry, I cannot confirm your request');
   }
 
-  restore();
+  restore(e);
+}
+
+async function decline(e) {
+  const divId = e?.dataset?.id;
+  const el = document.getElementById(divId);
+  const info = await respond(e);
+
+  const { name, guests, phone, message, db: { short } } = info;
+  const id = ID(phone);
+
+  let salutation = 'Your';
+
+  if (short)
+    salutation = `Dear ${short}, your`;
+
+  try {
+    await makeRequest('POST', `/decline/${id}`, {
+      message: `${message || ''} -- Name: ${name} -- Phone: ${phone} -- Guests: ${guests}`,
+    });
+    page(el, 'declined', 'declined-content', `
+      <h1 class='name'>${name}</h1>
+      <p>
+        ${salutation} presence will be missed at our wedding!
+        Let's keep in touch.
+      </p>
+
+      <p>
+        Love,<br/>
+        Paul & Kaylee
+      </p>
+    `);
+  } catch (er) {
+    page(el, 'error', 'error-content', `
+      ${salutation} answer can't be recorded.
+      It's ok, we will miss you though.
+    `);
+  }
+
+  restore(e);
 }
 
