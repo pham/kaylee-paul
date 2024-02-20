ad73e3e7520f9283a89a26e176a822a62f86f3bb
Paul Pham <paul@aquaron.com>
2024-02-19 16:43:30 -0800

    feat: Guest list update with CSV download

--- assets/js/guests.js
+++ assets/js/guests.js
@@ -5,10 +5,20 @@ function list(info) {
   const arr = [];
 
   for (const id in data) {
-    const { name,confirmed,guests,type,invited,doubtful } = data[id];
-    arr.push({
-      name, id, guests, confirmed, type, invited, doubtful
-    });
+    const { name,confirmed,type,company,phone } = data[id];
+    if (!company) continue;
+
+    for (const guest of company) {
+      const kid = guest.match(/\*$/) ? true : false;
+      arr.push({
+        name: guest.replace(/\*$/,''),
+        id,
+        kid,
+        type,
+        phone,
+        confirmed,
+      });
+    }
   }
 
   const sort = (a,b) => {
@@ -16,6 +26,8 @@ function list(info) {
     const bt = b.type || 5;
     return at < bt ? -1
       : at > bt ? 1
+      : a.id < b.id ? -1
+      : a.id > b.id ? 1
       : a.name.localeCompare(b.name);
   };
 
@@ -30,38 +42,57 @@ function draw(data) {
   const el = _el('list');
 
   let accepted = 0;
-  let declined = 0;
   let remained = 0;
+  let children = 0;
 
-  for (const {name,confirmed,invited,doubtful,guests,id} of info) {
-    const icon = confirmed ? 'check'
-      : confirmed === false ? 'close'
-      : invited ? 'schedule' : '';
+  for (const {name,confirmed,id,phone,kid} of info) {
+    const icon = kid ? 'child_care' : confirmed ? 'check' : '';
     el.innerHTML += `
-      <tr>
+      <tr id='${id}'>
         <td><span class='material-symbols-outlined'>${icon}</span></td>
         <th><a href='/rsvp#${id}'>${name}</a></th>
-        <td><code>${id}</code></td>
-        <td>${guests || ''}</td>
+        <td><code>${phone}</code></td>
       </tr>
     `;
 
-    const count = parseInt(guests || 0);
-
-    if (confirmed === undefined && !doubtful) {
-      remained += count || 0;
+    if (confirmed === undefined) {
+      remained++;
     } else if (confirmed) {
-      accepted += count;
-    } else {
-      declined += count;
+      accepted++;
     }
+    if (kid) children ++;
   }
 
   _el('_accepted').innerText = accepted;
-  _el('_declined').innerText = declined;
   _el('_remained').innerText = remained;
+  _el('_children').innerText = children;
   _el('_total').innerText = remained + accepted;
   _el('stats').hidden = false;
+}
+
+async function download(fname='guests.csv') {
+  const passcode = sessionStorage.getItem('bearer');
+  const info = await list(await aget('/list', passcode));
+  if (!info) return false;
+
+  const data = [];
+  for (const {name,type,phone,kid} of info) {
+    data.push([name,type,phone,kid ? 'kid' : ''].join(','));
+  }
+
+  const csvData = new Blob( ['\ufeff'+data.join("\n") ], {
+    type: 'text/csv;charset=utf-8;'
+  });
+  const csvURL = navigator.msSaveBlob
+    ? navigator.msSaveBlob (csvData,fname)
+    : window.URL.createObjectURL(csvData);
+
+  const link = document.createElement('a');
+  link.href = csvURL;
+  link.setAttribute('download', fname);
+  link.click();
+  link.remove();
+  return true;
 }
 
 async function callback(pw) {
